abf9c71a877463fcc537e8ea644a806f2c28d715
Odilon Ferreira <odilon@bridge.ufsc.br>
2022-11-11 12:34:43 -0300

    some pages and lodash lib

---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "bold-ui";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <div>
+        <Button>oi</Button>
+      </div>
+      <div></div>
+    </div>
+  );
+}
